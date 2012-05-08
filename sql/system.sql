7024cc168428959ca62097b2eb8bd40322e0d1ff
Pierre Queinnec <pierre.queinnec@zenika.com>
2012-05-08 19:26:30 +0200

    Use three broker instances in the tests.sql file.

--- sql/system.sql
+++ sql/system.sql
@@ -1,5 +1,7 @@
 -- enable the JVM to connect to all your RabbitMQ instances or load-balancers
+call dbms_java.grant_permission( 'HR', 'SYS:java.net.SocketPermission', '192.168.56.1:5671', 'connect,resolve' );
 call dbms_java.grant_permission( 'HR', 'SYS:java.net.SocketPermission', '192.168.56.1:5672', 'connect,resolve' );
+call dbms_java.grant_permission( 'HR', 'SYS:java.net.SocketPermission', '192.168.56.1:5673', 'connect,resolve' );
 
 -- tell the Oracle JVM to prefer IPv4 over IPv6 (if needed)...
 call dbms_java.grant_permission( 'HR', 'SYS:java.util.PropertyPermission', 'java.net.preferIPv4Stack', 'write' );
--- sql/tests.sql
+++ sql/tests.sql
@@ -1,6 +1,11 @@
--- print System.{out|err}
+-- print System.{out|err} if debugging
 SET SERVEROUTPUT ON
 CALL dbms_java.set_output(5000);
+
+-- define the broker instances
+insert into broker (BROKER_ID,HOST,PORT,VHOST,USERNAME,PASSWORD) values (1,'192.168.56.1',5671,'/','guest','guest');
+insert into broker (BROKER_ID,HOST,PORT,VHOST,USERNAME,PASSWORD) values (1,'192.168.56.1',5672,'/','guest','guest');
+insert into broker (BROKER_ID,HOST,PORT,VHOST,USERNAME,PASSWORD) values (1,'192.168.56.1',5673,'/','guest','guest');
 
 -- tests
 call amqp_exchange_declare(1, 'oracle', 'fanout');
