b19e2f8e4c30d625776321a56bf4a991346ca0bb
Pierre Queinnec <pierre.queinnec@zenika.com>
2012-05-09 00:05:16 +0200

    Remove the dbms_java.set_property call because a proper DBA would cringe at my attempt to execute that.

--- sql/system.sql
+++ sql/system.sql
@@ -6,12 +6,8 @@ call dbms_java.grant_permission( 'HR', 'SYS:java.net.SocketPermission', '192.168
 -- tell the Oracle JVM to prefer IPv4 over IPv6 (if needed)...
 call dbms_java.grant_permission( 'HR', 'SYS:java.util.PropertyPermission', 'java.net.preferIPv4Stack', 'write' );
 
--- ... then execute this as your user
-DECLARE
-   dummy VARCHAR2(4000);
-BEGIN
-   dummy := dbms_java.set_property('java.net.preferIPv4Stack', 'true');
-END;
+-- the code takes care of setting the property, but only at initialization, so you may need to reload the classes
+-- or it should be possible to use dbms_java.set_property('java.net.preferIPv4Stack', 'true');
 
 -- for remote Java debugging and fun
 -- grant DEBUG ANY PROCEDURE to hr;
